65610f9d6dda15347766a7a83759e96d63e01f26
Abel Fernández <abel.fernandez.nandin@gmail.com>
2022-11-09 09:15:41 +0100

    Add WalletProvider

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,13 +1,26 @@
 "use client";
 
 import { ChakraProvider } from "@chakra-ui/react";
+import { wallets } from "@cosmos-kit/keplr";
+import { WalletProvider } from "@cosmos-kit/react";
+import { assets, chains } from "chain-registry";
 import { RecoilRoot } from "recoil";
+import ConnectWalletModal from "../components/ConnectWalletModal";
 import { extendedTheme } from "../theme";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ChakraProvider theme={extendedTheme}>
-      <RecoilRoot>{children}</RecoilRoot>
+      <RecoilRoot>
+        <WalletProvider
+          chains={chains}
+          assetLists={assets}
+          wallets={wallets}
+          walletModal={ConnectWalletModal}
+        >
+          {children}
+        </WalletProvider>
+      </RecoilRoot>
     </ChakraProvider>
   );
 }
