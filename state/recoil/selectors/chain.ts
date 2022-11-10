a5fb2bcbffddae5696da0cc325cdf5fdb9a26dce
Abel Fernández <abel.fernandez.nandin@gmail.com>
2022-11-10 08:17:17 +0100

    Add coinByDenomSelector

--- state/recoil/selectors/chain.ts
+++ state/recoil/selectors/chain.ts
@@ -1,7 +1,27 @@
-import { selector } from "recoil";
-import { cosmWasmClientRouter, CHAIN_RPC_ENDPOINT } from "../../../utils";
+import { Coin } from "cosmwasm";
+import { selector, selectorFamily } from "recoil";
+import { CHAIN_RPC_ENDPOINT, cosmWasmClientRouter } from "../../../utils";
 
 export const cosmWasmClientSelector = selector({
   key: "cosmWasmClient",
   get: () => cosmWasmClientRouter.connect(CHAIN_RPC_ENDPOINT),
+});
+
+export const coinByDenomSelector = selectorFamily<
+  Coin,
+  {
+    readonly address?: string | undefined;
+    readonly denom?: string | undefined;
+  }
+>({
+  key: "coinByDenom",
+  get:
+    ({ address, denom }) =>
+    async ({ get }) => {
+      if (!address || !denom) return { denom: "", amount: "0" };
+
+      const client = get(cosmWasmClientSelector);
+      const coin = await client.getBalance(address, denom);
+      return coin;
+    },
 });
