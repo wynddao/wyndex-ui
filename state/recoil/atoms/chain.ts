55169ec6e3667066fce01ac3101fc989cc9a31f2
Abel Fernández <abel.fernandez.nandin@gmail.com>
2022-11-10 08:17:04 +0100

    Apply prettier to chain atoms

--- state/recoil/atoms/chain.ts
+++ state/recoil/atoms/chain.ts
@@ -1,15 +1,12 @@
-import { SigningCosmWasmClient } from 'cosmwasm'
-import { atom } from 'recoil'
+import { SigningCosmWasmClient } from "cosmwasm";
+import { atom } from "recoil";
 
-export const signingCosmWasmClientAtom = atom<
-  SigningCosmWasmClient | undefined
->({
-  key: 'signingCosmWasmClient',
+export const signingCosmWasmClientAtom = atom<SigningCosmWasmClient | undefined>({
+  key: "signingCosmWasmClient",
   dangerouslyAllowMutability: true,
-})
+});
 
 export const myAddressAtom = atom<number | null>({
-    key: "myAddress",
-    default: null,
-  });
-  
+  key: "myAddress",
+  default: null,
+});
