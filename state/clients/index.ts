2068c06c4454af9fbd5f5f4fede03e366dfce13b
Milan Steiner <steiner.milan@hotmail.com>
2022-11-24 13:27:41 +0100

    remove debug

--- state/clients/index.ts
+++ state/clients/index.ts
@@ -1,3 +1,4 @@
 export { WyndexFactoryQueryClient, WyndexFactoryClient } from "./WyndexFactory.client";
 export { WyndexPairQueryClient, WyndexPairClient } from "./WyndexPair.client";
 export { Cw20QueryClient, Cw20Client } from "./Cw20.client";
+export { CustomClient } from "./Custom.client";
