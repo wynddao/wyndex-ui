d6da2da1716664d567bb16bfcc266e96c3362496
Abel Fernández <abel.fernandez.nandin@gmail.com>
2022-11-28 14:07:54 +0100

    Remove unused component

--- components/IbcDepositModal/__mocks__/newPool.ts
+++
@@ -1,8 +0,0 @@
-import { dataType } from "..";
-import { OsmosisTokens } from "../../../utils/assets";
-
-export const poolOptionTokens: readonly dataType[] = OsmosisTokens.map(({ name, imgSrc }) => ({
-  label: name,
-  value: name,
-  imgSrc: imgSrc,
-}));
