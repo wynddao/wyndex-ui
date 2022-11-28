78412cc1178b043a8496d38d1b494c11ca8e55dd
Abel Fernández <abel.fernandez.nandin@gmail.com>
2022-11-28 14:07:48 +0100

    Add mock api and remove old one

--- utils/getData.ts
+++ utils/getData.ts
@@ -1,3 +1,5 @@
+import { Coin } from "cosmwasm";
+import { AssetsRecap } from "../components/AssetBalances";
 import { REST_API_ENDPOINT } from "./constants";
 import { Asset, Pair } from "./types";
 
@@ -25,6 +27,34 @@ export const getAsset = async (asset: string): Promise<Asset> => {
     .then((res) => res.json())
     .then((res) => {
       return res as Asset;
+    });
+};
+
+/**
+ * Get balance for all coins from address
+ *
+ * @param address
+ * @returns readonly Coin[]
+ */
+export const getBalances = async (address: string): Promise<readonly Coin[]> => {
+  return fetch(REST_API_ENDPOINT + "/balances/" + address)
+    .then((res) => res.json())
+    .then((res) => {
+      return res as readonly Coin[];
+    });
+};
+
+/**
+ * Get AssetsRecap from address
+ *
+ * @param address
+ * @returns AssetsRecap
+ */
+export const getAssetsRecap = async (address: string): Promise<AssetsRecap> => {
+  return fetch(REST_API_ENDPOINT + "/assetsRecap/" + address)
+    .then((res) => res.json())
+    .then((res) => {
+      return res as AssetsRecap;
     });
 };
 
