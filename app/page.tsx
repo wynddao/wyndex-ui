74307fd7f6dfa6fc384073628011b8b41b2d2089
Abel Fernández <abel.fernandez.nandin@gmail.com>
2022-11-28 14:09:27 +0100

    Adapt to AssetBalances props

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
 import AssetBalances from "../components/AssetBalances";
-import { assetsDetailsData, assetsTotalData } from "../components/AssetBalances/__mocks__/assets";
 import DepositIbcModal from "../components/DepositIbcModal";
 import WithdrawIbcModal from "../components/WithdrawIbcModal";
 
 export default function Page() {
   return (
     <>
-      <AssetBalances assetsDetailsData={assetsDetailsData} assetsTotalData={assetsTotalData} />
+      <AssetBalances />
       <DepositIbcModal />
       <WithdrawIbcModal />
     </>
