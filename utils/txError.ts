558d48ed82761a7f254d957f489710432ba76973
Kyaris <iris.ledesma.27@gmail.com>
2022-12-02 14:32:12 +0100

    feat: add txError format

---
+++ utils/txError.ts
@@ -0,0 +1,18 @@
+export class TxError extends Error {
+  constructor(msg: string) {
+    super(msg);
+    console.debug(msg);
+  }
+
+  pretty() {
+    const [msg] = this.message.split("Unknown desc =")[1].split("[CosmWasm");
+    if (msg.includes("contract: ")) return this.formatContractErr(msg);
+    // Support new cases
+    return "Something wen't wrong";
+  }
+
+  private formatContractErr(msg: string): string {
+    this.cause = "contract";
+    return msg.split("contract: ")[1];
+  }
+}
