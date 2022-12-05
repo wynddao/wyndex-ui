7c654ea6cbf8cd09ae2414bf31e652be278dc34c
Kyaris <iris.ledesma.27@gmail.com>
2022-12-05 15:14:21 +0100

    create custom success toast

---
+++ components/TxToast/TxToastSuccess.tsx
@@ -0,0 +1,31 @@
+import { Box, Link } from "@chakra-ui/react";
+import { ExecuteResult } from "cosmwasm";
+import React from "react";
+import { toast } from "react-hot-toast";
+
+export default function TxToastSuccess({ tx }: { tx: ExecuteResult }) {
+  return (
+    <Box display="flex" gap="1rem" minW="fit-content" justifyContent="center" alignItems="center">
+      <Box display="flex" flexFlow="column" paddingX="0.5rem">
+        Tx Success
+        <Link href={tx.transactionHash} isExternal color="gray.500">
+          See transaction
+        </Link>
+      </Box>
+      <Box
+        as="button"
+        onClick={() => toast.dismiss("tx.success")}
+        pl="1rem"
+        borderLeft="2px solid"
+        borderLeftColor="gray.200"
+        height="100%"
+        _hover={{
+          color: "brand.green.100",
+        }}
+        transition="all linear 0.5s"
+      >
+        Close
+      </Box>
+    </Box>
+  );
+}
