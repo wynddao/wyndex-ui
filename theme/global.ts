368fddc046717dac0862a53f2d5a880f12a1d400
Kyaris <iris.ledesma.27@gmail.com>
2022-12-06 17:06:01 +0100

    replace colors and export light and dark theme

---
+++ theme/global.ts
@@ -0,0 +1,17 @@
+export const styles = {
+  global: {
+    body: {
+      bg: "wynd.base.background",
+      color: "wynd.base.text",
+    },
+    a: {
+      color: "teal.500",
+      _hover: {
+        textDecoration: "underline",
+      },
+    },
+    button: {
+      outline: "none",
+    },
+  },
+};
