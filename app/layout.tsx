e8bff6c21d6d06e8c0d954b7a6f1f51f91aef562
Kyaris <iris.ledesma.27@gmail.com>
2022-12-07 22:24:27 +0100

    add logo icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <title>WynDex</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="Frontend for WynDex" />
+        <link rel="icon" type="image/svg" href="/logo-gradient.svg"></link>
       </head>
       <body>
         {isLoaded ? (
