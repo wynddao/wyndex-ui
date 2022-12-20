388bb166c77264c3abd553dfce8ba1cda28518c4
Kyaris <iris.ledesma.27@gmail.com>
2022-12-20 17:25:23 +0100

    support new err

--- utils/txError.ts
+++ utils/txError.ts
@@ -6,21 +6,28 @@ export class TxError extends Error {
 
   pretty() {
     const parsedMsg = this.parser();
+    console.log(parsedMsg[0]);
     if (!Array.isArray(parsedMsg)) return parsedMsg;
     const [msg] = parsedMsg;
-    if (msg.includes("contract: ")) return this.formatContractErr(msg);
+
     // Support new cases
+    if (msg.includes("contract: ")) return this.formatContractErr(msg);
+    if (msg.includes("exceeds max spread limit")) return "Operation exceeds max spread limit.";
+
     return "Something wen't wrong";
   }
 
   parser() {
-    console.log(this.message);
     const match = this.re.exec(this.message);
+
+    if (this.message.includes("Unknown desc =")) {
+      return this.message.split("Unknown desc =")[1]?.split("[CosmWasm");
+    }
+
     if (match != null) {
       return match[1];
     }
-    if (this.message.includes("Unknown desc ="))
-      return this.message.split("Unknown desc =")[1]?.split("[CosmWasm");
+
     return "Something wen't wrong";
   }
 
