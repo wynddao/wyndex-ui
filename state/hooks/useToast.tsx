ee9baa6c18d3a9a4f5d44f86038b05418dbbf411
Kyaris <iris.ledesma.27@gmail.com>
2022-12-20 16:41:30 +0100

    add own loading for toast

--- state/hooks/useToast.tsx
+++ state/hooks/useToast.tsx
@@ -1,21 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { toast } from "react-hot-toast";
 import { ExecuteResult } from "cosmwasm";
 import { TxError } from "../../utils/txError";
 import { TxToastSuccess } from "../../components/TxToast";
 
 export const useToast = () => {
+  const [isTxLoading, setIsTxLoading] = useState<boolean>(false);
   const txToast = async (fn: Function, ...args: unknown[]): Promise<ExecuteResult | undefined> => {
     try {
       return await toast.promise(
-        fn(...args),
+        (() => {
+          setIsTxLoading(true);
+          return fn(...args);
+        })(),
         {
           loading: "Loading...",
           success: (tx: ExecuteResult) => {
+            setIsTxLoading(false);
             toast.dismiss("tx.loading");
             return <TxToastSuccess tx={tx} />;
           },
           error: (err) => {
+            setIsTxLoading(false);
             toast.dismiss("tx.loading");
             return `Error: ${new TxError(err).pretty()}`;
           },
@@ -46,6 +52,7 @@ export const useToast = () => {
         },
       );
     } catch (err) {
+      setIsTxLoading(false);
       toast.error(`${err}`, {
         id: "tx.error",
         style: {
@@ -57,6 +64,7 @@ export const useToast = () => {
   };
 
   return {
+    isTxLoading,
     txToast,
   };
 };
