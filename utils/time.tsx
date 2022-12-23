68dc52ce0f43108f55d22b69a4c5a594a29a8b93
Kyaris <iris.ledesma.27@gmail.com>
2022-12-23 10:52:46 +0100

    add todo improvement

--- utils/time.tsx
+++ utils/time.tsx
@@ -1,5 +1,6 @@
 import { intervalToDuration } from "date-fns";
 
+//TODO: replace that method for date-fns method instead on places where is being used and delete that file.
 export function secondsToDays(seconds: number): number {
   const epoch = new Date(0);
   const secondsAfterEpoch = new Date(seconds * 1000);
