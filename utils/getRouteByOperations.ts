41bf21924cd32de70f28be84e51e05676923cc82
Milan Steiner <steiner.milan@hotmail.com>
2022-12-30 15:46:14 +0100

    Fixing swap route

--- utils/getRouteByOperations.ts
+++ utils/getRouteByOperations.ts
@@ -7,8 +7,8 @@ export const getRouteByOperations = (operations: SwapOperation[]) => {
       const from = Object.values(wyndex_swap.offer_asset_info)[0];
       const to = Object.values(wyndex_swap.ask_asset_info)[0];
       return {
-        from: wyndex_swap.ask_asset_info.hasOwnProperty("token") ? getAssetByTokenAddr(from)?.denom : from,
-        to: wyndex_swap.offer_asset_info.hasOwnProperty("token") ? getAssetByTokenAddr(to)?.denom : to,
+        from: wyndex_swap.offer_asset_info.hasOwnProperty("token") ? getAssetByTokenAddr(from)?.denom : from,
+        to: wyndex_swap.ask_asset_info.hasOwnProperty("token") ? getAssetByTokenAddr(to)?.denom : to,
       };
     })
     .reverse();
