82f668f9dc460af2b15f087ea99032f42691ae15
Milan Steiner <steiner.milan@hotmail.com>
2022-12-30 15:36:07 +0100

    Remove mystery box from navbar

--- components/Sidebar/SidebarContent.tsx
+++ components/Sidebar/SidebarContent.tsx
@@ -1,4 +1,4 @@
-"use client";;
+"use client";
 import { Box, BoxProps, CloseButton, Flex } from "@chakra-ui/react";
 import Image from "next/image";
 import { FiActivity, FiPackage, FiPieChart, FiRefreshCw, FiUsers } from "react-icons/fi";
@@ -14,8 +14,8 @@ const LinkItems: readonly LinkItemProps[] = [
   { name: "Dashboard", to: "/", icon: FiActivity },
   { name: "Swap", to: "/swap", icon: FiRefreshCw },
   { name: "Pools", to: "/pools", icon: FiPieChart },
+  { name: "Gauges", to: "https://app.wynddao.com/gauges", icon: FiUsers, isExternalLink: true },
   { name: "Governance", to: "https://app.wynddao.com", icon: FiUsers, isExternalLink: true },
-  { name: "Upcoming...", to: "/mystery-box", icon: FiPackage },
 ];
 
 interface SidebarContentProps extends BoxProps {
