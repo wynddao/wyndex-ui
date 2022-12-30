ce93ef348d957a292b7da8eda0351ac7936eac80
Milan Steiner <steiner.milan@hotmail.com>
2022-12-30 15:43:41 +0100

    Fix route

--- utils/getRouteByOperations.ts
+++ utils/getRouteByOperations.ts
@@ -4,8 +4,8 @@ import { getAssetByTokenAddr } from "./assets";
 export const getRouteByOperations = (operations: SwapOperation[]) => {
   return operations
     .map(({ wyndex_swap }) => {
-      const from = Object.values(wyndex_swap.ask_asset_info)[0];
-      const to = Object.values(wyndex_swap.offer_asset_info)[0];
+      const from = Object.values(wyndex_swap.offer_asset_info)[0];
+      const to = Object.values(wyndex_swap.ask_asset_info)[0];
       return {
         from: wyndex_swap.ask_asset_info.hasOwnProperty("token") ? getAssetByTokenAddr(from)?.denom : from,
         to: wyndex_swap.offer_asset_info.hasOwnProperty("token") ? getAssetByTokenAddr(to)?.denom : to,
