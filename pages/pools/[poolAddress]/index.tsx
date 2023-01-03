a78f51c7d198339ed6ad7b2650be7b0fd5cf2089
Milan Steiner <steiner.milan@hotmail.com>
2023-01-03 11:20:06 +0100

    Fix build & pools page

--- pages/pools/[poolAddress]/index.tsx
+++ pages/pools/[poolAddress]/index.tsx
@@ -1,6 +1,6 @@
 import Pool from "../../../components/Pool";
-import { useRouter } from "next/router";
-import { GetStaticProps, GetStaticPropsContext } from "next";
+import { GetStaticPaths, GetStaticPropsContext } from "next";
+import { INDEXER_API_ENDPOINT } from "../../../utils";
 
 export default function Page({ poolAddress }: { poolAddress: string }) {
   return <Pool poolAddress={poolAddress} />;
@@ -14,3 +14,19 @@ export async function getStaticProps(context: GetStaticPropsContext) {
     },
   };
 }
+
+export const getStaticPaths: GetStaticPaths<{ poolAddress: string }> = async () => {
+  const pools = await (await fetch(`${INDEXER_API_ENDPOINT}/pools`)).json();
+  const paths = Object.keys(pools).map((poolAddress) => {
+    return {
+      params: {
+        poolAddress,
+      },
+    };
+  });
+
+  return {
+    paths, //indicates that no page needs be created at build time
+    fallback: "blocking", //indicates the type of fallback
+  };
+};
