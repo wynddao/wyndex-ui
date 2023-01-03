f4c4ba891e609fd141fee9e92334bd810e8db1aa
abefernan <44572727+abefernan@users.noreply.github.com>
2023-01-03 11:00:43 +0100

    Add token filter components

---
+++ components/AssetBalances/TokenFilter.tsx
@@ -0,0 +1,30 @@
+import { HStack, useRadioGroup } from "@chakra-ui/react";
+import { Dispatch, SetStateAction } from "react";
+import TokenFilterOption from "./TokenFilterOption";
+
+const tokenFilterOptions = ["All", "Native", "CW20"] as const;
+export type TokenFilterOptions = typeof tokenFilterOptions[number];
+
+interface TokenFilterProps {
+  readonly setFilterOption: Dispatch<SetStateAction<TokenFilterOptions>>;
+}
+
+export default function TokenFilter({ setFilterOption }: TokenFilterProps) {
+  const { getRootProps, getRadioProps } = useRadioGroup({
+    name: "token-type",
+    defaultValue: tokenFilterOptions[0],
+    onChange: (nextValue: TokenFilterOptions) => setFilterOption(nextValue),
+  });
+
+  return (
+    <HStack {...getRootProps()}>
+      {tokenFilterOptions.map((value) => {
+        return (
+          <TokenFilterOption key={value} {...getRadioProps({ value })}>
+            {value}
+          </TokenFilterOption>
+        );
+      })}
+    </HStack>
+  );
+}
