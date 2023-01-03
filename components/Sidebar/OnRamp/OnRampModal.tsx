7276640f1ae54440dde8cecc0d71dd4b7e608386
abefernan <44572727+abefernan@users.noreply.github.com>
2023-01-03 13:49:23 +0100

    Fix network selection

--- components/Sidebar/OnRamp/OnRampModal.tsx
+++ components/Sidebar/OnRamp/OnRampModal.tsx
@@ -14,10 +14,9 @@ export default function OnRampModal({ onClose, isOpen }: OnRampModalOptions) {
       <ModalOverlay />
       <ModalContent maxW={{ md: "2xl" }} borderRadius="2xl" p={2} mx={2} bgColor="wynd.base.subBg">
         <iframe
-          src={`https://app.kado.money/?apiKey=b4995f6d-a443-4729-8c22-4f6ae70deb17&onPayCurrency=USD&onRevCurrency=USDC&product=BUY&networkList=JUNO&cryptoList=USDC&onToAddress=${walletAddress ?? ""}`}
-          width="100%"
-          height="700"
-          style={{ border: 0 }}
+          src={`https://app.kado.money/?apiKey=b4995f6d-a443-4729-8c22-4f6ae70deb17&onPayCurrency=USD&onRevCurrency=USDC&product=BUY&network=JUNO&cryptoList=USDC&onToAddress=${
+            walletAddress ?? ""
+          }`}
         ></iframe>
       </ModalContent>
     </Modal>
