2b71015181df32cd5c53c95f9bb2c7163b1e64bd
abefernan <44572727+abefernan@users.noreply.github.com>
2023-01-03 13:49:59 +0100

    Tweak styles and add close button

--- components/Sidebar/OnRamp/OnRampModal.tsx
+++ components/Sidebar/OnRamp/OnRampModal.tsx
@@ -1,5 +1,4 @@
-"use client";
-import { Modal, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, Text } from "@chakra-ui/react";
+import { Modal, ModalCloseButton, ModalContent, ModalOverlay } from "@chakra-ui/react";
 import { useWallet } from "@cosmos-kit/react";
 
 interface OnRampModalOptions {
@@ -12,11 +11,23 @@ export default function OnRampModal({ onClose, isOpen }: OnRampModalOptions) {
   return (
     <Modal isOpen={isOpen} onClose={onClose} isCentered={true}>
       <ModalOverlay />
-      <ModalContent maxW={{ md: "2xl" }} borderRadius="2xl" p={2} mx={2} bgColor="wynd.base.subBg">
+      <ModalContent
+        maxW={{ md: "2xl" }}
+        borderRadius="2xl"
+        p={2}
+        mx={2}
+        bgColor="white"
+        height="100%"
+        maxHeight="724px"
+      >
+        <ModalCloseButton sx={{ color: "black", marginRight: 2 }} />
         <iframe
+          title="Kado app"
           src={`https://app.kado.money/?apiKey=b4995f6d-a443-4729-8c22-4f6ae70deb17&onPayCurrency=USD&onRevCurrency=USDC&product=BUY&network=JUNO&cryptoList=USDC&onToAddress=${
             walletAddress ?? ""
           }`}
+          height={700}
+          style={{ marginTop: "24px" }}
         ></iframe>
       </ModalContent>
     </Modal>
