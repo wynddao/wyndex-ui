080888b4ca99d71f1b01fdbdefea838525fcdb33
abefernan <44572727+abefernan@users.noreply.github.com>
2023-01-19 17:36:58 +0100

    Use separate tokens refresh in Sidebar

--- components/Sidebar/ConnectWalletButton.tsx
+++ components/Sidebar/ConnectWalletButton.tsx
@@ -1,26 +1,37 @@
-"use client";
-
 import { Box, Button, Flex, Icon, Text, Tooltip, useClipboard } from "@chakra-ui/react";
 import { useWallet } from "@cosmos-kit/react";
 import { useEffect } from "react";
 import { FiCopy, FiCreditCard } from "react-icons/fi";
 import { VscDebugDisconnect } from "react-icons/vsc";
-import { useRecoilValueLoadable } from "recoil";
-import { coinByDenomSelector } from "../../state";
+import { useRecoilValue } from "recoil";
+import { useIndexerInfos } from "../../state";
+import { getAssetByDenom } from "../../utils/assets";
+import { microamountToAmount, microdenomToDenom } from "../../utils/tokens";
 
 export default function ConnectWalletButton() {
   const { onCopy, hasCopied, setValue } = useClipboard("");
-  const { address, openView, isWalletConnected, username, disconnect, currentChainRecord } = useWallet();
+  const {
+    address: walletAddress,
+    openView,
+    isWalletConnected,
+    username,
+    disconnect,
+    currentChainRecord,
+  } = useWallet();
+  const { ibcBalanceSelector, cw20BalanceSelector } = useIndexerInfos({});
 
-  const loadableBalance = useRecoilValueLoadable(
-    coinByDenomSelector({ address, serializedChainRecord: JSON.stringify(currentChainRecord) }),
+  const feeAsset = getAssetByDenom(currentChainRecord?.assetList?.assets[0].base || "");
+  const feeBalance = useRecoilValue(
+    "token_address" in feeAsset
+      ? cw20BalanceSelector(feeAsset.token_address)
+      : ibcBalanceSelector(feeAsset.denom),
   );
 
   useEffect(() => {
-    setValue(address || "");
-  }, [address, setValue]);
+    setValue(walletAddress || "");
+  }, [setValue, walletAddress]);
 
-  return isWalletConnected && loadableBalance ? (
+  return isWalletConnected ? (
     <Button
       bgGradient="linear(to-l, wynd.green.400, wynd.cyan.400)"
       _hover={{
@@ -38,9 +49,7 @@ export default function ConnectWalletButton() {
           <Box fontSize="sm">
             <Text>{username}</Text>
             <Text>
-              {loadableBalance.state === "hasValue"
-                ? `${Number(loadableBalance.contents.amount).toFixed(2)} ${loadableBalance.contents.denom}`
-                : "0"}
+              {microamountToAmount(feeBalance, feeAsset.decimals, 2)} {microdenomToDenom(feeAsset.denom)}
             </Text>
           </Box>
         </Flex>
