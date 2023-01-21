91c06f1b92a66db3f9e5881c8f926af4a729bf9a
Milan Steiner <steiner.milan@hotmail.com>
2023-01-21 18:45:38 +0100

    Fix build

--- components/AssetBalances/AssetBalanceItem.tsx
+++ components/AssetBalances/AssetBalanceItem.tsx
@@ -132,7 +132,7 @@ export default function AssetBalanceItem({ asset, toggleFav }: AssetBalanceItemP
             >
               IBC Withdraw
             </Button>
-          </>
+          </Flex>
         ) : (
           <Text></Text>
         )}
