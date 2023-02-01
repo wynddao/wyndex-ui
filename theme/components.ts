d653f9f5666dbe5dfd8725e8b1856de65ca83c9e
Milan Steiner <steiner.milan@hotmail.com>
2023-02-01 12:43:08 +0100

    Add default modal bgColor

--- theme/components.ts
+++ theme/components.ts
@@ -1,4 +1,14 @@
 import { StepsStyleConfig as Steps } from "chakra-ui-steps";
 import { ProgressTheme } from "../components/Dao/Vote/Progress";
 
-export const components = { Steps, ...ProgressTheme };
+export const components = {
+  Steps,
+  ...ProgressTheme,
+  Modal: {
+    baseStyle: {
+      dialog: {
+        bgColor: "wynd.base.subBg",
+      },
+    },
+  },
+};
