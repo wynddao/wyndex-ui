a641144112c258b467a83afb3c3172b0d7fe1769
WYNerD <102463473+WYNerD@users.noreply.github.com>
2023-03-16 20:22:19 +0100

    Update index.tsx

--- pages/lsd/index.tsx
+++ pages/lsd/index.tsx
@@ -1,6 +1,6 @@
 import { Box } from "@chakra-ui/react";
 import Head from "next/head";
-import MysteryBoxContainer from "../../components/Dex/MysteryBox";
+import { LsdOverview } from "../../components/Lsd/Overview";
 
 const Page = () => (
   <>
@@ -8,7 +8,7 @@ const Page = () => (
       <title>WYND | Liquid Staking Derivatives</title>
     </Head>
     <Box p="4">
-      <MysteryBoxContainer />
+      <LsdOverview />
     </Box>
   </>
 );
