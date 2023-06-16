5f7f96baf731967f93df1984e9c37f27ab1db3b2
Milan Steiner <steiner.milan@hotmail.com>
2023-06-16 12:25:29 +0200

    Fix type error

--- components/Dex/Pool/util/apr.ts
+++ components/Dex/Pool/util/apr.ts
@@ -1,7 +1,7 @@
 export const getApr = (
   apr: {
     unbonding_period: number;
-    apr: string;
+    apr: number | string;
   }[],
   unbonding_duration: number,
 ): string => {
@@ -9,7 +9,7 @@ export const getApr = (
   let res = "-";
 
   if (thisApr) {
-    res = thisApr;
+    res = thisApr.toString();
   } else {
     res = "-";
   }
